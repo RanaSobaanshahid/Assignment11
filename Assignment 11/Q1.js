e575c9fa4daa9ace96d3fadecb892d47bbe12d56
RanaSobaanshahid <124592143+RanaSobaanshahid@users.noreply.github.com>
2023-04-10 20:20:42 +0500

    Add files via upload

---
+++ Assignment 11/Q1.js
@@ -0,0 +1,27 @@
+
+// 1. Write a JavaScript program to list the properties of a JavaScript object
+
+// var student = {
+
+// name : "David Rayy",
+
+// sclass : "VI",
+
+// rollno : 12 };
+
+// Sample Output: name,sclass,rollno
+
+
+var student = {
+    name : "David Rayy",
+    sclass : "VI",
+    rollno : 12 
+   };
+    
+    console.log(student.name); console.log(student.sclass); console.log(student.rollno);
+   
+{
+    var type = typeof obj;
+    return type === 'function' || type === 'object' && !!obj;
+  }
+
---
+++ Assignment 11/Q10.js
@@ -0,0 +1,20 @@
+// 10. Write a JavaScript program to sort an array of JavaScript objects.  Sample Object :    var library = [     {         title:  'The Road Ahead',         author: 'Bill Gates',         libraryID: 1254     },     {         title: 'Walter Isaacson',         author: 'Steve Jobs',         libraryID: 4264     },     {         title: 'Mockingjay: The Final Book of The Hunger Games',         author: 'Suzanne Collins',         libraryID: 3245     }];  Expected Output:    [[object Object] {    author: "Walter Isaacson",    libraryID: 4264,    title: "Steve Jobs"  }, [object Object] {    author: "Suzanne Collins",    libraryID: 3245,    title: "Mockingjay: The Final Book of The Hunger Games"  }, [object Object] {    author: "The Road Ahead",    libraryID: 1254,    title: "Bill Gates"  }]
+
+
+var library = [ 
+    { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+    { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+    { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+    ];
+ 
+ function compare_to_sort(x,y) 
+  {
+   if (x.title < y.title)
+     return -1;
+   if (x.title > y.title)
+     return 1;
+   return 0;
+  }
+ 
+ console.log(library.sort(compare_to_sort));
+ 
